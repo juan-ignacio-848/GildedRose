2521647ca9fe6c24573693a9c476e1e17f3e8f0f
nmkip <juan.ignacio848@gmail.com>
2018-09-25 08:55:08 -0300

    Intermediate refactor

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -12,16 +12,27 @@ public class GildedRose {
     public void updateQuality()
     {
         for (Item item : items) {
-            updateAn(item);
+            QualityControl qualityControl = createQualityControlFor(item);
+            qualityControl.updateQualityFor(item);
+            updateSellInFor(item);
+            qualityControl.updateQualityForExpired(item);
         }
     }
 
-    private void updateAn(Item item) {
-        updateQualityFor(item);
+    private QualityControl createQualityControlFor(Item item) {
+        if("Aged Brie".equals(item.getName())) {
+            return new AgedBrieQualityControl();
+        }
 
-        updateSellInFor(item);
+        if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
+            return new BackStagePassesQualityControl();
+        }
 
-        updateQualityForExpired(item);
+        if ("Sulfuras, Hand of Ragnaros".equals(item.getName())) {
+            return new SulfurasQualityControl();
+        }
+
+        return new DefaultQualityControl();
     }
 
     private void updateSellInFor(Item item) {
@@ -30,12 +41,39 @@ public class GildedRose {
         }
     }
 
-    private void updateQualityFor(Item item) {
-        if (("Aged Brie".equals(item.getName()))) {
+    private class QualityControl {
+
+        protected void updateQualityFor(Item item) {
+
+        }
+
+        protected void updateQualityForExpired(Item item) {
+
+        }
+    }
+
+    private class AgedBrieQualityControl extends QualityControl {
+
+        @Override
+        protected void updateQualityFor(Item item) {
             if (item.getQuality() < 50) {
                 item.setQuality(item.getQuality() + 1);
             }
-        } else if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
+        }
+
+        @Override
+        protected void updateQualityForExpired(Item item) {
+            if(item.sellIn < 0) {
+                if (item.getQuality() < 50) {
+                    item.setQuality(item.getQuality() + 1);
+                }
+            }
+        }
+    }
+
+    private class BackStagePassesQualityControl extends QualityControl {
+        @Override
+        protected void updateQualityFor(Item item) {
             if (item.getQuality() < 50) {
                 item.setQuality(item.getQuality() + 1);
 
@@ -51,27 +89,43 @@ public class GildedRose {
                     }
                 }
             }
-        } else if (!"Sulfuras, Hand of Ragnaros".equals(item.getName())) {
-            if (item.getQuality() > 0) {
-                item.setQuality(item.getQuality() - 1);
+        }
+
+        @Override
+        protected void updateQualityForExpired(Item item) {
+            if(item.sellIn < 0) {
+                if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
+                    item.setQuality(0);
+                }
             }
         }
     }
 
-    private void updateQualityForExpired(Item item) {
-        if (item.getSellIn() < 0) {
-            if ("Aged Brie".equals(item.getName())) {
-                if (item.getQuality() < 50) {
-                    item.setQuality(item.getQuality() + 1);
-                }
-            } else if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
-                item.setQuality(0);
-            } else if (!"Sulfuras, Hand of Ragnaros".equals(item.getName())) {
+    private class SulfurasQualityControl extends QualityControl {
+        @Override
+        protected void updateQualityFor(Item item) {
+        }
+
+        @Override
+        protected void updateQualityForExpired(Item item) {
+        }
+    }
+
+    private class DefaultQualityControl extends QualityControl {
+        @Override
+        protected void updateQualityFor(Item item) {
+            if (item.getQuality() > 0) {
+                item.setQuality(item.getQuality() - 1);
+            }
+        }
+
+        @Override
+        protected void updateQualityForExpired(Item item) {
+            if (item.getSellIn() < 0) {
                 if (item.getQuality() > 0) {
                     item.setQuality(item.getQuality() - 1);
                 }
             }
         }
     }
-
 }
