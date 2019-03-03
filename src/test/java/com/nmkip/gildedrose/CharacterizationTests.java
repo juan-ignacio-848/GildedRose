0bcf751d97575e2ee73b5ac9c75d619c185ef1fb
nmkip <juan.ignacio848@gmail.com>
2019-03-03 11:20:07 -0300

    "Aged Brie" sellIn -1, quality +2 when sellIn = 0 "Aged Brie" sellIn -1, quality +1 when sellIn > 0 "Sulfuras Hand of Ragnaros" sellIn +0, quality +0 always "Backstage passes to a TAFKAL80ETC concert" sellIn -1, quality +1

--- src/test/java/com/nmkip/gildedrose/CharacterizationTests.java
+++ src/test/java/com/nmkip/gildedrose/CharacterizationTests.java
@@ -9,6 +9,7 @@ public class CharacterizationTests {
 
 	private static final String AGED_BRIE = "Aged Brie";
     private static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
+	private static final String BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
 
 	@Test
 	public void aged_brie_sellIn_decreases_by_1_and_quality_increases_by_1_when_sellIn_is_greater_than_0() {
@@ -54,5 +55,21 @@ public class CharacterizationTests {
         assertThat(sulfurasHandOfRagnaros.sellIn, is(20));
         assertThat(sulfurasHandOfRagnaros.quality, is(20));
     }
+
+	@Test
+	public void backstage_passes_sellIn_decreases_by_1_and_quality_increases_by_1() {
+		Item backstagePasses = new Item(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT, 30, 25);
+		Item[] items = new Item[] {
+				backstagePasses
+		};
+
+		GildedRose gildedRose = new GildedRose(items);
+
+		gildedRose.updateQuality();
+
+		assertThat(backstagePasses.sellIn, is(29));
+		assertThat(backstagePasses.quality, is(26));
+	}
+
 
 }
