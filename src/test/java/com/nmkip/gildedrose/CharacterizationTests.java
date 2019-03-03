75a1a36d8e24bfa72ef2676b5422ff086669c864
nmkip <juan.ignacio848@gmail.com>
2019-03-03 15:18:09 -0300

    Renamed stuff for clarity

--- src/test/java/com/nmkip/gildedrose/CharacterizationTests.java
+++ src/test/java/com/nmkip/gildedrose/CharacterizationTests.java
@@ -9,172 +9,159 @@ public class CharacterizationTests {
 
     private static final String AGED_BRIE = "Aged Brie";
     private static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
-	private static final String BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
     private static final String JUST_A_NORMAL_ITEM = "Just a normal item";
 
     @Test
-	public void aged_brie_sellIn_decreases_by_1_and_quality_increases_by_1_when_sellIn_is_greater_than_0() {
-		Item agedBrie = new Item(AGED_BRIE, 20, 20);
-		Item[] items = new Item[] {
-			agedBrie
-		};
+    public void aged_brie_sellIn_decreases_by_1_and_quality_increases_by_1_when_sellIn_is_greater_than_0() {
+        int sellIn = 20;
+        int quality = 20;
+        Item agedBrie = new Item(AGED_BRIE, sellIn, quality);
+        Item[] items = new Item[]{agedBrie};
 
-		GildedRose gildedRose = new GildedRose(items);
+        GildedRose gildedRose = new GildedRose(items);
 
-		gildedRose.updateQuality();
+        gildedRose.updateQuality();
 
-		assertThat(agedBrie.sellIn, is(19));
-		assertThat(agedBrie.quality, is(21));
-	}
+        assertThat(agedBrie.sellIn, is(sellIn - 1));
+        assertThat(agedBrie.quality, is(quality + 1));
+    }
 
-	@Test
-	public void aged_brie_sellIn_decreases_by_1_and_quality_increases_by_1_when_sellIn_is_less_or_equal_to_0() {
-		Item agedBrie = new Item(AGED_BRIE, -1, 20);
-		Item[] items = new Item[] {
-				agedBrie
-		};
+    @Test
+    public void aged_brie_sellIn_decreases_by_1_and_quality_increases_by_1_when_sellIn_is_less_or_equal_to_0() {
+        int sellIn = -1;
+        int quality = 20;
+        Item agedBrie = new Item(AGED_BRIE, sellIn, quality);
+        Item[] items = new Item[]{agedBrie};
+        GildedRose gildedRose = new GildedRose(items);
 
-		GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
 
-		gildedRose.updateQuality();
+        assertThat(agedBrie.sellIn, is(sellIn - 1));
+        assertThat(agedBrie.quality, is(quality + 2));
+    }
 
-		assertThat(agedBrie.sellIn, is(-2));
-		assertThat(agedBrie.quality, is(22));
-	}
+    @Test
+    public void aged_brie_quality_can_increase_up_to_50() {
+        int sellIn = 20;
+        int quality = 50;
+        Item agedBrie = new Item(AGED_BRIE, sellIn, quality);
+        Item[] items = new Item[]{agedBrie};
+        GildedRose gildedRose = new GildedRose(items);
 
-	@Test
-	public void aged_brie_quality_can_increase_up_to_50() {
-		Item agedBrie = new Item(AGED_BRIE, 20, 50);
-		Item[] items = new Item[] {
-				agedBrie
-		};
+        gildedRose.updateQuality();
 
-		GildedRose gildedRose = new GildedRose(items);
-
-		gildedRose.updateQuality();
-
-		assertThat(agedBrie.sellIn, is(19));
-		assertThat(agedBrie.quality, is(50));
-	}
+        assertThat(agedBrie.sellIn, is(sellIn - 1));
+        assertThat(agedBrie.quality, is(quality));
+    }
 
     @Test
     public void sulfuras_hand_of_ragnaros_quality_and_sellIn_remain_unchanged() {
-        Item sulfurasHandOfRagnaros = new Item(SULFURAS_HAND_OF_RAGNAROS, 20, 20);
-        Item[] items = new Item[] {
-                sulfurasHandOfRagnaros
-        };
-
+        int sellIn = 20;
+        int quality = 20;
+        Item sulfurasHandOfRagnaros = new Item(SULFURAS_HAND_OF_RAGNAROS, sellIn, quality);
+        Item[] items = new Item[]{sulfurasHandOfRagnaros};
         GildedRose gildedRose = new GildedRose(items);
 
         gildedRose.updateQuality();
 
-        assertThat(sulfurasHandOfRagnaros.sellIn, is(20));
-        assertThat(sulfurasHandOfRagnaros.quality, is(20));
+        assertThat(sulfurasHandOfRagnaros.sellIn, is(sellIn));
+        assertThat(sulfurasHandOfRagnaros.quality, is(quality));
     }
 
-	@Test
-	public void backstage_passes_sellIn_decreases_by_1_and_quality_increases_by_1_when_sellIn_greater_than_11() {
-		Item backstagePasses = new Item(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT, 30, 25);
-		Item[] items = new Item[] {
-				backstagePasses
-		};
+    @Test
+    public void backstage_passes_sellIn_decreases_by_1_and_quality_increases_by_1_when_sellIn_greater_than_11() {
+        Item backstagePasses = new Item(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT, 30, 25);
+        Item[] items = new Item[]{backstagePasses};
+        GildedRose gildedRose = new GildedRose(items);
 
-		GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
 
-		gildedRose.updateQuality();
+        assertThat(backstagePasses.sellIn, is(29));
+        assertThat(backstagePasses.quality, is(26));
+    }
 
-		assertThat(backstagePasses.sellIn, is(29));
-		assertThat(backstagePasses.quality, is(26));
-	}
+    @Test
+    public void backstage_passes_sellIn_decreases_by_1_and_quality_increases_by_2_when_sellIn_less_than_11() {
+        int sellIn = 10;
+        int quality = 25;
+        Item backstagePasses = new Item(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT, sellIn, quality);
+        Item[] items = new Item[]{backstagePasses};
+        GildedRose gildedRose = new GildedRose(items);
 
-	@Test
-	public void backstage_passes_sellIn_decreases_by_1_and_quality_increases_by_2_when_sellIn_less_than_11() {
-		Item backstagePasses = new Item(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT, 10, 25);
-		Item[] items = new Item[] {
-				backstagePasses
-		};
+        gildedRose.updateQuality();
 
-		GildedRose gildedRose = new GildedRose(items);
+        assertThat(backstagePasses.sellIn, is(sellIn - 1));
+        assertThat(backstagePasses.quality, is(quality + 2));
+    }
 
-		gildedRose.updateQuality();
+    @Test
+    public void backstage_passes_sellIn_decreases_by_1_and_quality_increases_by_3_when_sellIn_less_than_6() {
+        int sellIn = 5;
+        int quality = 25;
+        Item backstagePasses = new Item(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT, sellIn, quality);
+        Item[] items = new Item[]{backstagePasses};
+        GildedRose gildedRose = new GildedRose(items);
 
-		assertThat(backstagePasses.sellIn, is(9));
-		assertThat(backstagePasses.quality, is(27));
-	}
+        gildedRose.updateQuality();
 
-	@Test
-	public void backstage_passes_sellIn_decreases_by_1_and_quality_increases_by_3_when_sellIn_less_than_6() {
-		Item backstagePasses = new Item(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT, 5, 25);
-		Item[] items = new Item[] {
-				backstagePasses
-		};
+        assertThat(backstagePasses.sellIn, is(sellIn - 1));
+        assertThat(backstagePasses.quality, is(quality + 3));
+    }
 
-		GildedRose gildedRose = new GildedRose(items);
+    @Test
+    public void backstage_passes_sellIn_decreases_by_1_and_quality_drops_to_0_when_sellIn_is_less_or_equal_to_0() {
+        int sellIn = -1;
+        Item backstagePasses = new Item(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT, sellIn, 25);
+        Item[] items = new Item[]{backstagePasses};
+        GildedRose gildedRose = new GildedRose(items);
 
-		gildedRose.updateQuality();
+        gildedRose.updateQuality();
 
-		assertThat(backstagePasses.sellIn, is(4));
-		assertThat(backstagePasses.quality, is(28));
-	}
-
-	@Test
-	public void backstage_passes_sellIn_decreases_by_1_and_quality_drops_to_0_when_sellIn_is_less_or_equal_to_0() {
-		Item backstagePasses = new Item(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT, -1, 25);
-		Item[] items = new Item[] {
-				backstagePasses
-		};
-
-		GildedRose gildedRose = new GildedRose(items);
-
-		gildedRose.updateQuality();
-
-		assertThat(backstagePasses.sellIn, is(-2));
-		assertThat(backstagePasses.quality, is(0));
-	}
+        assertThat(backstagePasses.sellIn, is(sellIn - 1));
+        assertThat(backstagePasses.quality, is(0));
+    }
 
     @Test
     public void backstage_passes_quality_can_increase_up_to_50() {
-        Item backstagePasses = new Item(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT, 24, 50);
-        Item[] items = new Item[] {
-                backstagePasses
-        };
-
+        int sellIn = 24;
+        int quality = 50;
+        Item backstagePasses = new Item(BACKSTAGE_PASSES_TO_A_TAFKAL80ETC_CONCERT, sellIn, quality);
+        Item[] items = new Item[]{backstagePasses};
         GildedRose gildedRose = new GildedRose(items);
 
         gildedRose.updateQuality();
 
-        assertThat(backstagePasses.sellIn, is(23));
-        assertThat(backstagePasses.quality, is(50));
+        assertThat(backstagePasses.sellIn, is(sellIn - 1));
+        assertThat(backstagePasses.quality, is(quality));
     }
 
-	@Test
+    @Test
     public void normal_item_sellIn_decreases_by_1_and_quality_decreases_by_1_when_sellIn_is_greater_than_0() {
-        Item backstagePasses = new Item(JUST_A_NORMAL_ITEM, 5, 25);
-        Item[] items = new Item[] {
-                backstagePasses
-        };
-
+        int sellIn = 5;
+        int quality = 25;
+        Item normalItem = new Item(JUST_A_NORMAL_ITEM, sellIn, quality);
+        Item[] items = new Item[]{normalItem};
         GildedRose gildedRose = new GildedRose(items);
 
         gildedRose.updateQuality();
 
-        assertThat(backstagePasses.sellIn, is(4));
-        assertThat(backstagePasses.quality, is(24));
+        assertThat(normalItem.sellIn, is(sellIn - 1));
+        assertThat(normalItem.quality, is(quality - 1));
     }
 
     @Test
     public void normal_item_sellIn_decreases_by_1_and_quality_decreases_by_2_when_sellIn_is_less_or_equal_to_0() {
-        Item backstagePasses = new Item(JUST_A_NORMAL_ITEM, -1, 25);
-        Item[] items = new Item[] {
-                backstagePasses
-        };
-
+        int sellIn = -1;
+        int quality = 25;
+        Item normalItem = new Item(JUST_A_NORMAL_ITEM, sellIn, quality);
+        Item[] items = new Item[]{normalItem};
         GildedRose gildedRose = new GildedRose(items);
 
         gildedRose.updateQuality();
 
-        assertThat(backstagePasses.sellIn, is(-2));
-        assertThat(backstagePasses.quality, is(23));
+        assertThat(normalItem.sellIn, is(sellIn - 1));
+        assertThat(normalItem.quality, is(quality - 2));
     }
 
 }
