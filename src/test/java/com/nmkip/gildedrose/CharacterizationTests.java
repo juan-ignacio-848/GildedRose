fbc1af767799038603dac8e10b38d2e573b1a63c
nmkip <juan.ignacio848@gmail.com>
2019-03-03 11:12:43 -0300

    "Aged Brie" sellIn -1, quality +2 when sellIn = 0 "Aged Brie" sellIn -1, quality +1 when sellIn > 0 "Sulfuras Hand of Ragnaros" sellIn +0, quality +0 always

--- src/test/java/com/nmkip/gildedrose/CharacterizationTests.java
+++ src/test/java/com/nmkip/gildedrose/CharacterizationTests.java
@@ -8,6 +8,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class CharacterizationTests {
 
 	private static final String AGED_BRIE = "Aged Brie";
+    private static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
 
 	@Test
 	public void aged_brie_sellIn_decreases_by_1_and_quality_increases_by_1_when_sellIn_is_greater_than_0() {
@@ -38,4 +39,20 @@ public class CharacterizationTests {
 		assertThat(agedBrie.sellIn, is(-1));
 		assertThat(agedBrie.quality, is(22));
 	}
+
+    @Test
+    public void sulfuras_hand_of_ragnaros_quality_and_sellIn_remain_unchanged() {
+        Item sulfurasHandOfRagnaros = new Item(SULFURAS_HAND_OF_RAGNAROS, 20, 20);
+        Item[] items = new Item[] {
+                sulfurasHandOfRagnaros
+        };
+
+        GildedRose gildedRose = new GildedRose(items);
+
+        gildedRose.updateQuality();
+
+        assertThat(sulfurasHandOfRagnaros.sellIn, is(20));
+        assertThat(sulfurasHandOfRagnaros.quality, is(20));
+    }
+
 }
