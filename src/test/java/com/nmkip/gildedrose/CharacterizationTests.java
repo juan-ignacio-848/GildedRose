8cf979f62c2a15c1479345bc8b5561dac4f5c6c0
nmkip <juan.ignacio848@gmail.com>
2019-03-03 10:50:31 -0300

    Changed package structre.

--- src/test/java/com/nmkip/gildedrose/CharacterizationTests.java
+++ src/test/java/com/nmkip/gildedrose/CharacterizationTests.java
@@ -1,9 +1,11 @@
+package com.nmkip.gildedrose;
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 
-public class GildedRoseTest {
+public class CharacterizationTests {
 
 	@Test
 	public void testTheTruth() {
