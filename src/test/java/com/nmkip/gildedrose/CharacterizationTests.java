3e883429847e9bdbbfb862c12f21442e18ff9615
nmkip <juan.ignacio848@gmail.com>
2019-03-03 11:07:43 -0300

    "Aged Brie" sellIn -1, quality +2 when sellIn = 0 "Aged Brie" sellIn -1, quality +1 when sellIn > 0

--- src/test/java/com/nmkip/gildedrose/CharacterizationTests.java
+++ src/test/java/com/nmkip/gildedrose/CharacterizationTests.java
@@ -7,10 +7,10 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class CharacterizationTests {
 
-	public static final String AGED_BRIE = "Aged Brie";
+	private static final String AGED_BRIE = "Aged Brie";
 
 	@Test
-	public void aged_brie_quality_increases_by_1_while_sellIn_decreases_by_1() {
+	public void aged_brie_sellIn_decreases_by_1_and_quality_increases_by_1_when_sellIn_is_greater_than_0() {
 		Item agedBrie = new Item(AGED_BRIE, 20, 20);
 		Item[] items = new Item[] {
 			agedBrie
@@ -20,8 +20,22 @@ public class CharacterizationTests {
 
 		gildedRose.updateQuality();
 
-		assertThat(agedBrie.quality, is(21));
 		assertThat(agedBrie.sellIn, is(19));
+		assertThat(agedBrie.quality, is(21));
 	}
 
+	@Test
+	public void aged_brie_sellIn_decreases_by_1_and_quality_increases_by_1_when_sellIn_is_equal_to_0() {
+		Item agedBrie = new Item(AGED_BRIE, 0, 20);
+		Item[] items = new Item[] {
+				agedBrie
+		};
+
+		GildedRose gildedRose = new GildedRose(items);
+
+		gildedRose.updateQuality();
+
+		assertThat(agedBrie.sellIn, is(-1));
+		assertThat(agedBrie.quality, is(22));
+	}
 }
