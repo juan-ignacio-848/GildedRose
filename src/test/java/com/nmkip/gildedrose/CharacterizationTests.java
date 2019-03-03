cd272b1b273be8fc0bfb041eccc96c85491122c8
nmkip <juan.ignacio848@gmail.com>
2019-03-03 10:58:58 -0300

    "Aged Brie" quality increases by 1 while sellIn decreases by 1.

--- src/test/java/com/nmkip/gildedrose/CharacterizationTests.java
+++ src/test/java/com/nmkip/gildedrose/CharacterizationTests.java
@@ -1,14 +1,27 @@
 package com.nmkip.gildedrose;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class CharacterizationTests {
 
+	public static final String AGED_BRIE = "Aged Brie";
+
 	@Test
-	public void testTheTruth() {
-		assertTrue(true);
+	public void aged_brie_quality_increases_by_1_while_sellIn_decreases_by_1() {
+		Item agedBrie = new Item(AGED_BRIE, 20, 20);
+		Item[] items = new Item[] {
+			agedBrie
+		};
+
+		GildedRose gildedRose = new GildedRose(items);
+
+		gildedRose.updateQuality();
+
+		assertThat(agedBrie.quality, is(21));
+		assertThat(agedBrie.sellIn, is(19));
 	}
+
 }
